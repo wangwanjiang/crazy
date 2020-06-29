5d41434dc291b54376004e631ce1d98d5e527bbb
Your Name <you@example.com>
2020-06-29 18:02:02 +0800

    first commit

---
+++ src/test/java/com/wwj/crazy/CrazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwj.crazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
